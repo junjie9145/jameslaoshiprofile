17d7fcf4448fa62e184b7babac45e0d9e23e176c
Junjie <james9145@hotmail.com>
2021-07-26 12:38:26 +0800

    fixed links in html

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import _ from "lodash";
 import slider from "./pictureSlider";
 import navSlide from "./navslide";
+import "../node_modules/video.js/dist/video.min";
+import "../node_modules/videojs-youtube/dist/Youtube.min"
 
 const init = (() => {
     navSlide();
